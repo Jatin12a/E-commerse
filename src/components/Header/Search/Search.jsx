cf251e327fe09ee52285cef0927fe06350d926e8
Jatin12a <gmusti890@gmail.com>
2023-11-18 00:34:41 +0530

    html part done
    by here i completed all the html part of my project

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,6 +1,29 @@
+import { MdClose } from 'react-icons/md';
+import prod from '../../../assets/products/earbuds-prod-4.webp'
+
 import "./Search.css";
-const Search = () => {
-    return <div>Search</div>;
+const Search = ({setshowsearch}) => {
+    return <div className='search-modal'>
+        <div className="form-field">
+            <input type="text" autoFocus placeholder='search here' />
+        <MdClose className='clos-btn' onClick={()=>{setshowsearch(false)}}/>
+        </div>
+        <div className="search-result">
+            <div className="results">
+                <div className="search-item">
+                    <div className="ssimg-cont">
+                    <img src={prod} alt="" />
+                    </div>
+                    <div className="ssprods-detail">
+                        <span className="ssname">product name</span>
+                        <span className="ssdesc">product desc</span>
+                    
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
 };
 
 export default Search;
